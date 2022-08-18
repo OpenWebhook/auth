f51e334b3e8cd9c04bd9b4147ee576431223f513
Samox <sammy.teillet@gmail.com>
2022-08-18 11:23:50 +0200

    feature(User): Store user in database

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,36 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../infrastructure/prisma.service';
+import { v4 as uuidv4 } from 'uuid';
+import { User } from '@prisma/client';
 
-export type User = {
-  email: string;
-  picture: string | null;
-  name: string | null;
-  password?: string;
-};
+export type UserInput = { email: string; name: string; picture: string };
 
 @Injectable()
 export class UsersService {
-  private readonly users = [
-    {
-      userId: 1,
-      name: 'john',
-      password: 'changeme',
-      email: 'email',
-      picture: 'croute.fr/picture',
-    },
-    {
-      userId: 2,
-      name: 'maria',
-      password: 'guess',
-      email: 'email',
-      picture: 'croute.fr/picture',
-    },
-  ];
+  constructor(private readonly prismaService: PrismaService) {}
 
-  async findOne(username: string): Promise<User | undefined> {
-    return this.users.find((user) => user.name === username);
+  async findOne(email: string): Promise<User | undefined> {
+    return this.prismaService.user.findUnique({ where: { email } });
   }
 
-  async findOrCreateUser(user: User): Promise<User> {
-    return user;
+  async findOrCreateUser(user: {
+    email: string;
+    name: string;
+    picture: string;
+  }): Promise<User> {
+    const storedUser = await this.findOne(user.email);
+    if (storedUser) {
+      return storedUser;
+    }
+    const id = `us-${uuidv4()}`;
+    const createdUser = await this.prismaService.user.create({
+      data: { ...user, id },
+    });
+    return createdUser;
   }
 }
