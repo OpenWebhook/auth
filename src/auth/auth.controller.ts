f1c531996232ae511b70f77914752e8d4eb5568d
Samox <sammy.teillet@gmail.com>
2022-09-12 19:36:10 +0200

    feature(JWK): USe redis as storage for keys

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,16 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { Response } from 'express';
 import * as jose from 'node-jose';
-import * as fs from 'fs';
 import { keyFileName } from './auth.module';
-import { AuthService } from './auth.service';
+import { RedisService } from '../infrastructure/redis.service';
 
 @Controller()
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly redisService: RedisService) {}
+
   @Get('.well-known/jwks.json')
   async getJWKeys(@Res() res: Response) {
-    const ks = fs.readFileSync(keyFileName);
+    const ks = await this.redisService.client.get(keyFileName);
 
     const keyStore = await jose.JWK.asKeyStore(ks.toString());
 
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,12 +5,13 @@ import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import * as fs from 'fs';
 import * as jose from 'node-jose';
 import { OAuthController } from './oauth.controller';
 import { HttpModule } from '@nestjs/axios';
 import { WebhookStoreAuthController } from './webhook-store.auth.service';
 import { JwtStrategy } from './jwt.strategy';
+import { RedisModule } from '../infrastructure/redis.module';
+import { RedisService } from '../infrastructure/redis.service';
 
 export const keyFileName = 'keys.json';
 
@@ -20,14 +21,19 @@ export const keyFileName = 'keys.json';
     PassportModule,
     ConfigModule,
     HttpModule,
+    RedisModule,
     JwtModule.registerAsync({
-      useFactory: async (): Promise<JwtModuleOptions> => {
+      imports: [RedisModule],
+      inject: [RedisService],
+      useFactory: async (
+        redisService: RedisService,
+      ): Promise<JwtModuleOptions> => {
         const keyStore = jose.JWK.createKeyStore();
         await keyStore.generate('RSA', 2048, { alg: 'RS256', use: 'sig' });
 
-        fs.writeFileSync(
+        redisService.client.set(
           keyFileName,
-          JSON.stringify(keyStore.toJSON(true), null, '  '),
+          JSON.stringify(keyStore.toJSON(true)),
         );
 
         const key = keyStore.toJSON(true).keys[0];
